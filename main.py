603177c5867d3773fdbd1ef87cc8c094a64b6480
TischTasche <soeren-petersen@gmx.net>
2024-11-05 21:07:03 +0800

    Added imports

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import cv2
 import sys
+import os
+import glob
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 from PyQt5.QtWidgets import (
     QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QHBoxLayout,
     QGroupBox, QWidget, QLineEdit, QSpinBox
 )
 
-""" class MainWindow(QMainWindow):
+
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('cvdlhw1.ui')
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(QPushButton('Button')) """
+        self.setWindowTitle('cvdlhw1.ui - Sören Petersen')
+        self.setGeometry(100, 100, 1600, 1000)
+
+        layout = QGridLayout()
+
 
 app = QApplication([])
 label = QLabel('Hello PyQt5!')
